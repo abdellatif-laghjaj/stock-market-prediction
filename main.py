c6e6407af56767958304605ac7fe218e20fa6cd6
Naima ELJID <114741266+neimael@users.noreply.github.com>
2023-12-18 17:14:36 +0100

    plots

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from datetime import date
 import yfinance as yf
 from prophet import Prophet
 from prophet.plot import plot_plotly
-from services import load_data, plot_data
+from services import load_data, plot_data, plot_multiple_data
 
 # Set page layout to wide
 st.set_page_config(layout="wide")
@@ -20,6 +20,8 @@ st.markdown("<p style='text-align: center;'>A simple web app for stock price pre
 
 stocks = ("AAPL", "GOOG", "MSFT", "GME", "AMC", "TSLA", "FB", "AMZN", "NFLX", "NVDA", "AMD", "PYPL")
 selected_stock = st.sidebar.selectbox("Select stock for prediction", stocks)
+selected_stocks = st.sidebar.multiselect("Select stocks for comparaison", stocks)
+
 years_to_predict = st.sidebar.slider("Years of prediction:", 1, 5)
 period = years_to_predict * 365
 
@@ -67,3 +69,25 @@ st.markdown("<h2><span style='color: orange;'>{}</span> Forecast Components</h2>
 st.write("This section breaks down the forecast components, including trends and seasonality, for {}.".format(selected_stock))
 components = model.plot_components(forecast)
 st.write(components)
+
+# Forcast multiple stocks
+stocks_data = []
+forcasted_data = []
+for stock in selected_stocks:
+    stocks_data.append(load_data(stock, start_date, TODAY))
+
+st.markdown("<h2><span style='color: orange;'>{}</span> Forecast Plot of Multiple Stocks</h2>".format(', '.join(selected_stocks)), unsafe_allow_html=True)
+st.write("This section visualizes the forecasted stock price for {} using a time series plot.".format(', '.join(selected_stocks)))
+
+for data in stocks_data:
+    if data is not None:
+        df_train = data[["Date", "Close"]]
+        df_train = df_train.rename(columns={"Date": "ds", "Close": "y"})
+        model = Prophet()
+        model.fit(df_train)
+        future = model.make_future_dataframe(periods=period)
+        forecast = model.predict(future)
+        forecast = forecast[forecast['ds'] >= TODAY]
+        forcasted_data.append(forecast)
+
+plot_multiple_data(forcasted_data, selected_stocks)
--- services.py
+++ services.py
@@ -16,3 +16,10 @@ def plot_data(data):
     fig.add_trace(go.Scatter(x=data['Date'], y=data['Close'], name="stock_close"))
     fig.update_layout(title_text="Stock Prices Over Time", xaxis_rangeslider_visible=True)
     st.plotly_chart(fig, use_container_width=True)
+
+def plot_multiple_data(data, stock_names):
+    fig = go.Figure()
+    for i, stock_data in enumerate(data):
+        fig.add_trace(go.Scatter(x=stock_data['ds'], y=stock_data['yhat'], name=f"yhat - {stock_names[i]}"))
+    fig.update_layout(title_text="Stock Prices Over Time", xaxis_rangeslider_visible=True)
+    st.plotly_chart(fig, use_container_width=True)
