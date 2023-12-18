4899e369f03d914493e001fc6b2c5d8b7c7ab560
ABDELLATIF LAGHJAJ <abdelatiflaghjaj@gmail.com>
2023-12-18 14:38:16 +0100

    Update services.py

--- services.py
+++ services.py
@@ -1,6 +1,7 @@
-import yfinance as yf
 import streamlit as st
 from plotly import graph_objs as go
+import yfinance as yf
+
 
 @st.cache_data
 def load_data(ticker, start, end):
@@ -8,10 +9,10 @@ def load_data(ticker, start, end):
     data.reset_index(inplace=True)
     return data
 
-def plot_raw_data(data):
-    print(data)
+
+def plot_data(data):
     fig = go.Figure()
     fig.add_trace(go.Scatter(x=data['Date'], y=data['Open'], name="stock_open"))
     fig.add_trace(go.Scatter(x=data['Date'], y=data['Close'], name="stock_close"))
-    fig.layout.update(title_text="Time Series Data", xaxis_rangeslider_visible=True)
-    st.plotly_chart(fig)
+    fig.update_layout(title_text="Time Series Data", xaxis_rangeslider_visible=True)
+    st.plotly_chart(fig, use_container_width=True)
