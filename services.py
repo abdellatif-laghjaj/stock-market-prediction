aa5a5df379566d6626870206b3e2a1b06313b8ba
ABDELLATIF LAGHJAJ <abdelatiflaghjaj@gmail.com>
2023-12-23 15:45:15 +0100

    volume plot

--- services.py
+++ services.py
@@ -49,3 +49,15 @@ def plot_multiple_data(data, stock_names):
         fig.add_trace(go.Scatter(x=stock_data['ds'], y=stock_data['yhat'], name=f"yhat - {stock_names[i]}"))
     fig.update_layout(title_text="Stock Prices Over Time", xaxis_rangeslider_visible=True)
     st.plotly_chart(fig, use_container_width=True)
+
+def plot_volume(data):
+    """
+    Plot historical stock volume data.
+
+    Parameters:
+    - data (pd.DataFrame): DataFrame containing historical stock volume data.
+    """
+    fig = go.Figure()
+    fig.add_trace(go.Scatter(x=data['Date'], y=data['Volume'], name="stock_volume"))
+    fig.update_layout(title_text="Stock Volume Over Time", xaxis_rangeslider_visible=True)
+    st.plotly_chart(fig, use_container_width=True)
